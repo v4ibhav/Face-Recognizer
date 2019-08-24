d315e5560eb2212bfae12538d91e9736796e244b
v4ibhav <51675640+v4ibhav@users.noreply.github.com>
2019-08-25 00:58:56 +0530

    Delete harcascade_try.py

--- harcascade_try.py
+++
@@ -1,9 +0,0 @@
-#we read and show an image
-import pandas as pd 
-import cv2
-img = cv2.imread('../../../Codes/Jupyter Notebook/Pokemon/Images/296.jpg')
-cv2.imshow('pikachu',img)
-gray = cv2.imread('../../../Codes/Jupyter Notebook/Pokemon/Images/296.jpg',cv2.IMREAD_GRAYSCALE)
-cv2.imshow('greyed_out',gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
